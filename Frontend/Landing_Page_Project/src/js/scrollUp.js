f27c190b9b35ee2ad2997a059ed6f70c4411c337
PrasathVjKmr <prasathvjkmr@gmail.com>
2023-01-24 21:36:15 +0530

    ScrollUp

---
+++ Frontend/Landing_Page_Project/src/js/scrollUp.js
@@ -0,0 +1,9 @@
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
